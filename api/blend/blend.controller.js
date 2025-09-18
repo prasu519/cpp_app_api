52ce0fd960b701c4cea0e13df6e4dfc7c085d8df
prasu519 <chengalaprasanna@gmail.com>
2025-09-18 12:19:44 +0530

    cpp3 blend work done

--- api/blend/blend.controller.js
+++ api/blend/blend.controller.js
@@ -1,6 +1,7 @@
 const {
   blendServicePost,
   blendServiceGet,
+  blendServiceGetCpp3,
   blendServiceGetDatewise,
   blendServiceGetDatewiseCpp3,
   blendServicePostCpp3,
@@ -43,6 +44,23 @@ module.exports = {
     const selectedDate = req.query.date;
     const selectedShift = req.query.shift;
     blendServiceGet(selectedDate, selectedShift, (error, results) => {
+      if (error) {
+        return res.status(500).json({
+          success: 0,
+          message: error,
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+
+  blendControllerGetCpp3: (req, res) => {
+    const selectedDate = req.query.date;
+    const selectedShift = req.query.shift;
+    blendServiceGetCpp3(selectedDate, selectedShift, (error, results) => {
       if (error) {
         return res.status(500).json({
           success: 0,
--- api/blend/blend.router.js
+++ api/blend/blend.router.js
@@ -2,6 +2,7 @@ const {
   blendControllerPost,
   blendControllerGet,
   blendControllerPostCpp3,
+  blendControllerGetCpp3,
 } = require("../blend/blend.controller");
 
 const blendRouter = require("express").Router();
@@ -9,5 +10,6 @@ const blendRouter = require("express").Router();
 blendRouter.post("/", blendControllerPost);
 blendRouter.post("/cpp3", blendControllerPostCpp3);
 blendRouter.get("/", blendControllerGet);
+blendRouter.get("/cpp3", blendControllerGetCpp3);
 
 module.exports = blendRouter;
